92196b2ffd0bdb55db0bc8acd4ae424e5822e3b3
Rodolfo soto <rod.sotod@duocuc.cl>
2025-05-13 00:13:13 -0400

    cambios

--- src/main/java/com/example/ecomarketgt/Repository/UsuariooRepository.java
+++ src/main/java/com/example/ecomarketgt/Repository/UsuariooRepository.java
@@ -1,5 +1,6 @@
 package com.example.ecomarketgt.Repository;
 
+import java.beans.BeanProperty;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +13,9 @@ import com.example.ecomarketgt.Modelo.Usuarioo;
 // interface que extiende de JpaRepository para realizar 
 public interface UsuariooRepository extends JpaRepository<Usuarioo,Long>{
     
+    @Query("SELECT p FROM Usuarioo p WHERE LOWER(email) LIKE LOWER(CONCAT('%',:email,'%'))")
     List<Usuarioo> findByEmail(String email);
+    
 
     @Query("SELECT p FROM Usuarioo p WHERE LOWER(nombres) LIKE LOWER(CONCAT('%',:nombre,'%'))")
     List<Usuarioo> findByNombres(String nombres);
