1791fa65d20e3cf71523ce10be53fdc48b11ba2a
Rodolfo soto <rod.sotod@duocuc.cl>
2025-05-21 16:37:32 -0400

    cambios funcionado ok  metodos

--- src/main/java/com/example/ecomarketgt/EcomarketgtApplication.java
+++ src/main/java/com/example/ecomarketgt/EcomarketgtApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.example.ecomarketgt") // Escanea el paquete base
 public class EcomarketgtApplication {
 
+    public EcomarketgtApplication() {
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EcomarketgtApplication.class, args);
     }
--- src/main/java/com/example/ecomarketgt/Repository/UsuariooRepository.java
+++ src/main/java/com/example/ecomarketgt/Repository/UsuariooRepository.java
@@ -8,19 +8,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
-import com.example.ecomarketgt.Modelo.Usuarioo;
+import com.example.ecomarketgt.Modelo.Usuarioo;// importando la clase Usuarioo
 @Repository
 // interface que extiende de JpaRepository para realizar 
 public interface UsuariooRepository extends JpaRepository<Usuarioo,Long>{
     
-    @Query("SELECT p FROM Usuarioo p WHERE LOWER(email) LIKE LOWER(CONCAT('%',:email,'%'))")
+    @Query("SELECT p FROM Usuarioo p WHERE LOWER(email) LIKE LOWER(CONCAT('%',:email,'%'))")  // consulta para buscar por email
     List<Usuarioo> findByEmail(String email);
 
 
-    @Query("SELECT p FROM Usuarioo p WHERE LOWER(nombres) LIKE LOWER(CONCAT('%',:nombres,'%'))")
+    @Query("SELECT p FROM Usuarioo p WHERE LOWER(nombres) LIKE LOWER(CONCAT('%',:nombres,'%'))") // consulta para buscar por nombres
     List<Usuarioo> findByNombres(String nombres);
 
-    @Query("SELECT p FROM Usuarioo p WHERE LOWER(rut) LIKE LOWER(CONCAT('%',:rut,'%'))")
+    @Query("SELECT p FROM Usuarioo p WHERE LOWER(rut) LIKE LOWER(CONCAT('%',:rut,'%'))") // consulta para buscar por rut
     List<Usuarioo> findByRut(String rut);
 
     
